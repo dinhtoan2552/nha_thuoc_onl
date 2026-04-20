6c498bbe8910ff7230d4591cf50d5b08cf807aec
dinhtoan2552 <dinhtoan204nd@gmail.com>
2026-04-20 16:12:28 +0700

    Update DBConnection.java

--- DBConnection.java
+++ DBConnection.java
@@ -3,61 +3,34 @@ package utils;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.Properties;
 
 public class DBConnection {
 
     private static final String URL =
-            "jdbc:mysql://localhost:3306/banthuoc"
-            + "?useSSL=false"
-            + "&serverTimezone=Asia/Ho_Chi_Minh"
-            + "&useUnicode=true"
-            + "&characterEncoding=UTF-8"
-            + "&autoReconnect=true";
+        "jdbc:mysql://shinkansen.proxy.rlwy.net:59349/railway"
+        + "?useSSL=false"
+        + "&serverTimezone=Asia/Ho_Chi_Minh"
+        + "&useUnicode=true"
+        + "&characterEncoding=UTF-8";
 
     private static final String USER = "root";
-    private static final String PASSWORD = "";
+    private static final String PASSWORD = "YOUR_PASSWORD"; // thay password trong ảnh
 
     static {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            System.out.println("Da nap MySQL JDBC Driver thanh cong!");
+            System.out.println("Load driver OK");
         } catch (ClassNotFoundException e) {
-            System.out.println("Khong tim thay MySQL JDBC Driver!");
             e.printStackTrace();
         }
     }
 
     public static Connection getConnection() {
-        Connection conn = null;
         try {
-            Properties props = new Properties();
-            props.setProperty("user", USER);
-            props.setProperty("password", PASSWORD);
-
-            conn = DriverManager.getConnection(URL, props);
-
-            conn.setAutoCommit(true);
-            conn.setTransactionIsolation(Connection.TRANSACTION_READ_COMMITTED);
-
-            System.out.println("Ket noi database thanh cong!");
+            return DriverManager.getConnection(URL, USER, PASSWORD);
         } catch (SQLException e) {
-            System.out.println("Ket noi database that bai!");
             e.printStackTrace();
-        }
-        return conn;
-    }
-
-    public static void closeConnection(Connection conn) {
-        if (conn != null) {
-            try {
-                if (!conn.isClosed()) {
-                    conn.close();
-                    System.out.println("Da dong ket noi database.");
-                }
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
+            return null;
         }
     }
 }
